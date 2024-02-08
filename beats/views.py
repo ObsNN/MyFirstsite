9c41b44eb310b5a772de9a1aa12113123c5275fb
Obscure <gameshd23@mail.ru>
2024-02-08 19:21:23 +0500

    posts

--- beats/views.py
+++ beats/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Music
+from django.utils import timezone
 
 def music(request):
-	return render(request, 'beats/music.html', {})
+	posts = Music.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'beats/music.html', {'posts': posts})
